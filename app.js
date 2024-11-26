7f864ee67c0f3d9c271aeae374e2a6e0fa14b4d0
Ariel-Joe <arieljoe@gmail.com>
2024-11-26 09:48:23 +0300

    express

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
-
 const path = require("path");
+const bodyParser = require("body-parser")
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json())
 
 app.use(express.static(path.join(__dirname, "public")))
 
